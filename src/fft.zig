a50128afbc0da5e16afb1574bd9b5d7b3d247545
Philipp Muens <philipp@muens.io>
2025-06-21 20:40:08 +0200

    Remove allocator usage in FFT implementation

--- src/fft.zig
+++ src/fft.zig
@@ -4,166 +4,142 @@
 const std = @import("std");
 const math = std.math;
 const testing = std.testing;
-const Allocator = std.mem.Allocator;
 const pi = math.pi;
 const Complex = math.Complex;
 
 const utils = @import("utils.zig");
 const expectEqualComplexSlices = @import("testing.zig").expectEqualComplexSlices;
 
-/// Instance of a Fast Fourier Transform based on n-th root of unity.
+/// Fast Fourier Transform based on n-th root of unity.
 /// The FFT algorithm is based on the paper "Low-Cost and Area-Efficient FPGA
 /// Implementations of Lattice-Based Cryptography" by Aysu et al.
 // See: https://schaumont.dyn.wpi.edu/schaum/pdf/papers/2013hostb.pdf
-pub const FFT = struct {
-    const Self = @This();
+pub fn FFT(comptime n: i64) type {
+    return struct {
+        const Self = @This();
 
-    /// Length of FFT vector.
-    n: i64,
-    /// Powers of omega from omega^0 to omega^(n - 1).
-    omega_powers: []Complex(f64),
-    /// Powers of omega^-1 = iomega from imoega^0 to iomega^(n - 1).
-    omega_inverse_powers: []Complex(f64),
-    /// Allocator used for internal memory allocations.
-    allocator: Allocator,
+        /// Length of FFT vector.
+        comptime n: i64 = n,
+        /// Powers of omega from omega^0 to omega^(n - 1).
+        omega_powers: [n]Complex(f64),
+        /// Powers of omega^-1 = iomega from imoega^0 to iomega^(n - 1).
+        omega_inverse_powers: [n]Complex(f64),
 
-    /// Initializes a new FFT instance with the given FFT vector length n.
-    pub fn init(allocator: Allocator, n: i64) !Self {
-        // Check if vector length n is a power of 2.
-        if (!utils.isPowerOfTwo(n)) {
-            return error.InvalidLength;
+        /// Initializes a new FFT instance. Ensures that the FFT vector length
+        /// is a power of 2.
+        pub fn init() !Self {
+            // Check if FFT vector length n is a power of 2.
+            if (!utils.isPowerOfTwo(n)) {
+                return error.InvalidLength;
+            }
+
+            // Compute powers of omega as well as powers of omega^-1.
+            var omega_powers = [_]Complex(f64){Complex(f64).init(0, 0)} ** n;
+            var omega_inverse_powers = [_]Complex(f64){Complex(f64).init(0, 0)} ** n;
+
+            for (0..n) |i| {
+                const angle = 2 * pi * @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(n));
+                omega_powers[i] = Complex(f64).init(@cos(angle), @sin(angle));
+                omega_inverse_powers[i] = Complex(f64).init(@cos(-angle), @sin(-angle));
+            }
+
+            return Self{
+                .n = n,
+                .omega_powers = omega_powers,
+                .omega_inverse_powers = omega_inverse_powers,
+            };
         }
 
-        // Compute powers of omega as well as powers of omega^-1.
-        var powers = try std.ArrayList(Complex(f64)).initCapacity(allocator, @intCast(n));
-        var powers_inverse = try std.ArrayList(Complex(f64)).initCapacity(allocator, @intCast(n));
-        defer powers.deinit();
-        defer powers_inverse.deinit();
+        /// Runs a forward pass of FFT with the given coefficients.
+        /// Note that the slice of coefficients is mutated in-place.
+        pub fn fwd(self: Self, coefficients: []Complex(f64)) ![]Complex(f64) {
+            // Length of coefficients must equal n.
+            if (coefficients.len != self.n) {
+                return error.InvalidLength;
+            }
 
-        for (0..@intCast(n)) |i| {
-            const angle = 2 * pi * @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(n));
-            const power = Complex(f64).init(@cos(angle), @sin(angle));
-            const power_inverse = Complex(f64).init(@cos(-angle), @sin(-angle));
-            try powers.append(power);
-            try powers_inverse.append(power_inverse);
-        }
-        const omega_powers = try powers.toOwnedSlice();
-        const omega_inverse_powers = try powers_inverse.toOwnedSlice();
-
-        return FFT{
-            .n = n,
-            .omega_powers = omega_powers,
-            .omega_inverse_powers = omega_inverse_powers,
-            .allocator = allocator,
-        };
-    }
-
-    /// Release all allocated memory.
-    pub fn deinit(self: Self) void {
-        self.allocator.free(self.omega_powers);
-        self.allocator.free(self.omega_inverse_powers);
-    }
-
-    /// Runs a forward pass of FFT with the given coefficients.
-    /// The caller owns the returned memory.
-    pub fn fwd(self: Self, coefficients: []const Complex(f64)) ![]const Complex(f64) {
-        // Length of coefficients must equal n.
-        if (coefficients.len != self.n) {
-            return error.InvalidLength;
+            return self.fft(coefficients, self.omega_powers);
         }
 
-        return self.fft(coefficients, self.omega_powers);
-    }
+        /// Runs iFFT (Inverse FFT) with the given coefficients.
+        /// Note that the slice of coefficients is mutated in-place.
+        pub fn inv(self: Self, coefficients: []Complex(f64)) ![]Complex(f64) {
+            // Length of coefficients must equal n.
+            if (coefficients.len != self.n) {
+                return error.InvalidLength;
+            }
 
-    /// Runs IFFT (Inverse FFT) with the given coefficients.
-    /// The caller owns the returned memory.
-    pub fn inv(self: Self, coefficients: []const Complex(f64)) ![]const Complex(f64) {
-        // Length of coefficients must equal n.
-        if (coefficients.len != self.n) {
-            return error.InvalidLength;
+            var result = try self.fft(coefficients, self.omega_inverse_powers);
+
+            const n_complex = Complex(f64).init(@floatFromInt(self.n), 0);
+            for (0..coefficients.len) |i| {
+                result[i] = result[i].div(n_complex);
+            }
+
+            return result;
         }
 
-        const unscaled_result = try self.fft(coefficients, self.omega_inverse_powers);
-        defer self.allocator.free(unscaled_result);
+        /// Runs an iterative version of FFT with the given coefficients and twiddles
+        /// which are the powers of the roots of unity (i.e. powers of omega / powers
+        /// of omega^-1).
+        /// Note that the slice of coefficients is mutated in-place.
+        fn fft(self: Self, coefficients: []Complex(f64), twiddles: [n]Complex(f64)) ![]Complex(f64) {
+            // Length of coefficients and twiddles must be the same.
+            if (coefficients.len != twiddles.len) {
+                return error.InvalidLength;
+            }
 
-        var result = try self.allocator.alloc(Complex(f64), coefficients.len);
+            const log2_n = std.math.log2_int(usize, @intCast(self.n));
+            var result = try utils.bitReverseSlice(Complex(f64), self.n, coefficients);
 
-        const n = Complex(f64).init(@floatFromInt(self.n), 0);
-        for (0..coefficients.len) |i| {
-            result[i] = unscaled_result[i].div(n);
-        }
+            for (0..log2_n) |i| {
+                const in_1 = @as(usize, 1) << @intCast(i); // 2^i
+                const in_2 = @as(usize, 1) << @intCast(i + 1); // 2^(i + 1)
+                const in_3 = @as(usize, @intCast(self.n)) >> @intCast(i + 1); // n >> (i + 1) = n / 2^(i + 1)
 
-        return result;
-    }
+                for (0..in_1) |j| {
+                    for (0..in_3) |t| {
+                        const index_even = (t * in_2) + j; // (t * 2^(i + 1)) + j
+                        const index_odd = index_even + in_1; // (t * 2^(i + 1)) + j + 2^i
 
-    /// Runs an iterative version of FFT with the given coefficients and twiddles
-    /// which are the powers of the roots of unity (i.e. powers of omega / powers
-    /// of omega^-1).
-    /// The caller owns the returned memory.
-    fn fft(self: Self, coefficients: []const Complex(f64), twiddles: []const Complex(f64)) ![]const Complex(f64) {
-        // Length of coefficients and twiddles must be the same.
-        if (coefficients.len != twiddles.len) {
-            return error.InvalidLength;
-        }
+                        const c = result[index_even];
+                        const d = result[index_odd];
 
-        const log2_n = std.math.log2_int(usize, @intCast(self.n));
-        const reversed = try utils.bitReverseSlice(Complex(f64), self.allocator, coefficients);
-        defer self.allocator.free(reversed);
+                        const twiddle_index = @as(usize, (j * @as(usize, @intCast(self.n)))) >> @intCast(i + 1); // (j * n) >> i + 1 = (j * n) / 2^(i + 1)
+                        const twiddle_factor = twiddles[twiddle_index].mul(d);
 
-        var result = try self.allocator.dupe(Complex(f64), reversed);
+                        const butterfly_plus = c.add(twiddle_factor);
+                        const butterfly_minus = c.sub(twiddle_factor);
 
-        for (0..log2_n) |i| {
-            const in_1 = @as(usize, 1) << @intCast(i); // 2^i
-            const in_2 = @as(usize, 1) << @intCast(i + 1); // 2^(i + 1)
-            const in_3 = @as(usize, @intCast(self.n)) >> @intCast(i + 1); // n >> (i + 1) = n / 2^(i + 1)
-
-            for (0..in_1) |j| {
-                for (0..in_3) |t| {
-                    const index_even = (t * in_2) + j; // (t * 2^(i + 1)) + j
-                    const index_odd = index_even + in_1; // (t * 2^(i + 1)) + j + 2^i
-
-                    const c = result[index_even];
-                    const d = result[index_odd];
-
-                    const twiddle_index = @as(usize, (j * @as(usize, @intCast(self.n)))) >> @intCast(i + 1); // (j * n) >> i + 1 = (j * n) / 2^(i + 1)
-                    const twiddle_factor = twiddles[twiddle_index].mul(d);
-
-                    const butterfly_plus = c.add(twiddle_factor);
-                    const butterfly_minus = c.sub(twiddle_factor);
-
-                    result[index_even] = butterfly_plus;
-                    result[index_odd] = butterfly_minus;
+                        result[index_even] = butterfly_plus;
+                        result[index_odd] = butterfly_minus;
+                    }
                 }
             }
-        }
 
-        return result;
-    }
-};
+            return result;
+        }
+    };
+}
 
 test "fft - init" {
-    const allocator = testing.allocator;
-
     {
         const n = 5;
 
         const expected = error.InvalidLength;
-        const result = FFT.init(allocator, n);
+        const result = FFT(n).init();
 
         try testing.expectError(expected, result);
     }
 }
 
 test "fft - fwd" {
-    const allocator = testing.allocator;
-
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(1, 0),
             Complex(f64).init(2, 0),
             Complex(f64).init(3, 0),
@@ -177,7 +153,6 @@ test "fft - fwd" {
         };
 
         const result = try fft.fwd(&coefficients);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -185,10 +160,9 @@ test "fft - fwd" {
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(5, 0),
             Complex(f64).init(6, 0),
             Complex(f64).init(7, 0),
@@ -202,7 +176,6 @@ test "fft - fwd" {
         };
 
         const result = try fft.fwd(&coefficients);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -210,10 +183,9 @@ test "fft - fwd" {
     {
         const n = 8;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(1, 0), //
             Complex(f64).init(1, 0),
             Complex(f64).init(1, 0),
@@ -235,7 +207,6 @@ test "fft - fwd" {
         };
 
         const result = try fft.fwd(&coefficients);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -243,10 +214,9 @@ test "fft - fwd" {
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(1, 0), //
             Complex(f64).init(2, 0),
             Complex(f64).init(3, 0),
@@ -260,15 +230,12 @@ test "fft - fwd" {
 }
 
 test "fft - inv" {
-    const allocator = testing.allocator;
-
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(10, 0), //
             Complex(f64).init(-2, -2),
             Complex(f64).init(-2, 0),
@@ -282,7 +249,6 @@ test "fft - inv" {
         };
 
         const result = try fft.inv(&coefficients);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -290,10 +256,9 @@ test "fft - inv" {
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(26, 0), //
             Complex(f64).init(-2, -2),
             Complex(f64).init(-2, 0),
@@ -307,7 +272,6 @@ test "fft - inv" {
         };
 
         const result = try fft.inv(&coefficients);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -315,10 +279,9 @@ test "fft - inv" {
     {
         const n = 8;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(4, 0), //
             Complex(f64).init(1, 2.41421),
             Complex(f64).init(0, 0),
@@ -340,7 +303,6 @@ test "fft - inv" {
         };
 
         const result = try fft.inv(&coefficients);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -348,10 +310,9 @@ test "fft - inv" {
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(1, 0), //
             Complex(f64).init(2, 0),
             Complex(f64).init(3, 0),
@@ -365,16 +326,13 @@ test "fft - inv" {
 }
 
 test "fft - fft" {
-    const allocator = testing.allocator;
-
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
         const twiddles = fft.omega_powers;
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(1, 0),
             Complex(f64).init(2, 0),
             Complex(f64).init(3, 0),
@@ -388,7 +346,6 @@ test "fft - fft" {
         };
 
         const result = try fft.fft(&coefficients, twiddles);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
     }
@@ -396,11 +353,10 @@ test "fft - fft" {
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
         const twiddles = fft.omega_powers;
-        const coefficients = [_]Complex(f64){
+        var coefficients = [_]Complex(f64){
             Complex(f64).init(1, 0), //
             Complex(f64).init(2, 0),
         };
@@ -413,21 +369,18 @@ test "fft - fft" {
 }
 
 test "fft - convolution" {
-    const allocator = testing.allocator;
-
     {
         const n = 4;
 
-        const fft = try FFT.init(allocator, n);
-        defer fft.deinit();
+        const fft = try FFT(n).init();
 
-        const coefficients_1 = [_]Complex(f64){
+        var coefficients_1 = [_]Complex(f64){
             Complex(f64).init(3, 0), //
             Complex(f64).init(2, 0),
             Complex(f64).init(0, 0),
             Complex(f64).init(0, 0),
         };
-        const coefficients_2 = [_]Complex(f64){
+        var coefficients_2 = [_]Complex(f64){
             Complex(f64).init(1, 0), //
             Complex(f64).init(5, 0),
             Complex(f64).init(0, 0),
@@ -445,8 +398,6 @@ test "fft - convolution" {
 
         const fwd_1 = try fft.fwd(&coefficients_1);
         const fwd_2 = try fft.fwd(&coefficients_2);
-        defer allocator.free(fwd_1);
-        defer allocator.free(fwd_2);
 
         var interim = [_]Complex(f64){Complex(f64).init(0, 0)} ** n;
         for (0..n) |i| {
@@ -454,18 +405,16 @@ test "fft - convolution" {
         }
 
         const result = try fft.inv(&interim);
-        defer allocator.free(result);
 
         try expectEqualComplexSlices(f64, &expected, result);
 
         // Modify result to turn complex number into integer.
         const expected_casted = [_]i64{ 3, 17, 10, 0 };
-        var result_casted = try allocator.alloc(i64, n);
-        defer allocator.free(result_casted);
+        var result_casted = [_]i64{0} ** n;
         for (0..n) |i| {
             result_casted[i] = @intFromFloat(result[i].re + 0.5);
         }
 
-        try testing.expectEqualSlices(i64, &expected_casted, result_casted);
+        try testing.expectEqualSlices(i64, &expected_casted, &result_casted);
     }
 }
